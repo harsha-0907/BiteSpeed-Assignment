30466ca5e3bb54441aa99ab98c08e6b03d8b5610
arakkutrip1612 <arakkutrip1612@gmail.com>
2025-08-13 12:47:48 +0000

    Refactored code to redirect user to docs for any other endpoint

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request as FastAPIRequest
+from fastapi.responses import JSONResponse, RedirectResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from model import Request
-from fastapi.responses import JSONResponse
 from logic import AppUsers
 
 app = FastAPI()
 
 @app.get("/")
 async def getServerRunningStatus():
-  return JSONResponse(
-    content={
-      "message":"Server is Running",
-      "endpoint-list": ["/identify", "/place-order"]
-    }, status_code=200
-  )
+    return JSONResponse(
+        content={
+            "message": "Server is Running",
+            "endpoint-list": ["/identify", "/place-order"]
+        }, status_code=200
+    )
 
 @app.post("/identify")
 def fetchContactData(requestData: Request):
-  email, phoneNumber = requestData.email, requestData.phoneNumber
-  data = AppUsers.findContact(requestData)
-  return JSONResponse(
-    content=data, status_code=200
-  )
+    email, phoneNumber = requestData.email, requestData.phoneNumber
+    data = AppUsers.findContact(requestData)
+    return JSONResponse(
+        content=data, status_code=200
+    )
 
+@app.exception_handler(StarletteHTTPException)
+async def custom_404_handler(request: FastAPIRequest, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        return RedirectResponse(url="/docs")
+    return JSONResponse(
+        content={"detail": exc.detail}, status_code=exc.status_code
+    )
 
 if __name__ == "__main__":
-  import uvicorn
-  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

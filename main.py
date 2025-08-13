1448209320dd4ce5446b67b439484cc585435b91
goodsirhello807 <goodsirhello807@gmail.com>
2025-08-13 10:00:27 +0000

    Added the server endpoints

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-import os
 
-from flask import Flask
+from fastapi import FastAPI
+from model import Request
+from fastapi.responses import JSONResponse
+from model import AppUsers
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/")
-def hello_world():
-  """Example Hello World route."""
-  name = os.environ.get("NAME", "World")
-  return f"Hello {name}!"
+@app.get("/")
+async def getServerRunningStatus():
+  return JSONResponse(
+    content={
+      "message":"Server is Running",
+      "endpoint-list": ["/identify", "/place-order"]
+    }, status_code=200
+  )
+
+@app.post("/identify")
+def fetchContactData(requestData: Request):
+  email, phoneNumber = requestData.email, requestData.phoneNumber
+  data = AppUsers.findContact(requestData)
+  return JSONResponse(
+    content=data, status_code=200
+  )
+
 
 if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+  import uvicorn
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
